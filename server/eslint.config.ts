f486f8f839651a39f7edfabafdcbb72534fbc2ae
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-20 00:59:12 +0700

    add Prettier and ESLint configuration files; create .prettierignore to exclude specific files

---
+++ server/eslint.config.ts
@@ -0,0 +1,41 @@
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsparser from '@typescript-eslint/parser'
+import prettier from 'eslint-plugin-prettier'
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier,
+    },
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: [
+      'node_modules',
+      'dist',
+      '.env',
+      '.DS_Store',
+      'src/graphql.type.ts',
+    ],
+  },
+]
