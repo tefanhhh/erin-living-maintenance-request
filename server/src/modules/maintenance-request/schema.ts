357705c12e652309656e612c57cedea7d19c781f
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-20 01:00:41 +0700

    refactor MaintenanceRequest schema for improved formatting and readability

--- server/src/modules/maintenance-request/schema.ts
+++ server/src/modules/maintenance-request/schema.ts
@@ -1,19 +1,38 @@
-import { ObjectId }  from 'mongodb';
-import { Schema, model } from 'mongoose';
-import { MaintenanceRequest, MaintenanceRequestStatus, MaintenanceRequestUrgency } from '../../graphql.type';
+import { ObjectId } from 'mongodb'
+import { Schema, model } from 'mongoose'
+import {
+  MaintenanceRequest,
+  MaintenanceRequestStatus,
+  MaintenanceRequestUrgency,
+} from '../../graphql.type'
 
-const schema = new Schema<MaintenanceRequest>({
-  _id: { type: ObjectId, required: true },
-  title: { type: String, required: true },
-  description: { type: String, required: false },
-  status: { type: String, enum: Object.values(MaintenanceRequestStatus), required: true },
-  urgency: { type: String, enum: Object.values(MaintenanceRequestUrgency), required: true },
-  createdAt: { type: String, required: true },
-  updatedAt: { type: String, required: true },
-  deletedAt: { type: String, required: false, default: null },
-}, {
-  timestamps: true,
-  versionKey: false,
-})
+const schema = new Schema<MaintenanceRequest>(
+  {
+    _id: { type: ObjectId, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    status: {
+      type: String,
+      enum: Object.values(MaintenanceRequestStatus),
+      required: true,
+    },
+    urgency: {
+      type: String,
+      enum: Object.values(MaintenanceRequestUrgency),
+      required: true,
+    },
+    createdAt: { type: String, required: true },
+    updatedAt: { type: String, required: true },
+    deletedAt: { type: String, required: false, default: null },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+)
 
-export const MaintenanceRequestSchema = model<MaintenanceRequest>('MaintenanceRequest', schema, 'maintenance_requests');
+export const MaintenanceRequestSchema = model<MaintenanceRequest>(
+  'MaintenanceRequest',
+  schema,
+  'maintenance_requests',
+)
