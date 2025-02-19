9b68444ee2c415aec52e38e6b5dd8fa22b874b54
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-20 00:59:24 +0700

    refactor codegen.ts for consistency in formatting and style

--- server/src/codegen.ts
+++ server/src/codegen.ts
@@ -1,14 +1,13 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "src/schema.graphql",
+  schema: 'src/schema.graphql',
   generates: {
-    "src/graphql.type.ts": {
-      plugins: ["typescript", "typescript-resolvers", "typescript-mongodb"]
-    }
-  }
-};
+    'src/graphql.type.ts': {
+      plugins: ['typescript', 'typescript-resolvers', 'typescript-mongodb'],
+    },
+  },
+}
 
-export default config;
+export default config
