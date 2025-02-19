ecbc4b617dae53f22e34e90c072f074c2141d1bc
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-20 01:00:51 +0700

    refactor MaintenanceRequestService for consistent formatting and improved readability

--- server/src/modules/maintenance-request/service.ts
+++ server/src/modules/maintenance-request/service.ts
@@ -1,53 +1,61 @@
-import { ObjectId } from "mongodb";
-import { MaintenanceRequestSchema } from "../../modules/maintenance-request/schema";
-import { MaintenanceRequest, MaintenanceRequestInput } from "../../graphql.type";
+import { ObjectId } from 'mongodb'
+import { MaintenanceRequestSchema } from '../../modules/maintenance-request/schema'
+import { MaintenanceRequest, MaintenanceRequestInput } from '../../graphql.type'
 
 export class MaintenanceRequestService {
-  private readonly schema = MaintenanceRequestSchema;
+  private readonly schema = MaintenanceRequestSchema
 
   async create(body: MaintenanceRequestInput): Promise<MaintenanceRequest> {
-    const date = new Date();
+    const date = new Date()
     const created = await this.schema.insertOne({
       ...body,
       _id: new ObjectId(),
       createdAt: date,
       updatedAt: date,
-    });
-    return await this.findOne(created._id);
+    })
+    return await this.findOne(created._id)
   }
 
-  async update(_id: string, body: MaintenanceRequestInput): Promise<MaintenanceRequest> {
-    const date = new Date();
-    await this.schema.updateOne({
-      _id,
-    }, {
-      $set: {
-        ...body,
-        updatedAt: date,
-      }
-    })
-    return await this.findOne(_id);
+  async update(
+    _id: string,
+    body: MaintenanceRequestInput,
+  ): Promise<MaintenanceRequest> {
+    const date = new Date()
+    await this.schema.updateOne(
+      {
+        _id,
+      },
+      {
+        $set: {
+          ...body,
+          updatedAt: date,
+        },
+      },
+    )
+    return await this.findOne(_id)
   }
 
   async findOne(id: string): Promise<MaintenanceRequest> {
-    const maintenanceRequest = await this.schema.findById(id);
-    return maintenanceRequest;
-
+    const maintenanceRequest = await this.schema.findById(id)
+    return maintenanceRequest
   }
 
   async findAll(): Promise<MaintenanceRequest[]> {
-    const maintenanceRequests = await this.schema.find();
-    return maintenanceRequests;
+    const maintenanceRequests = await this.schema.find()
+    return maintenanceRequests
   }
 
   async delete(_id: string): Promise<boolean> {
-    const result = await this.schema.updateOne({
-      _id,
-    }, {
-      $set: {
-        deletedAt: new Date()
+    const result = await this.schema.updateOne(
+      {
+        _id,
       },
-    })
-    return result.modifiedCount > 0;
+      {
+        $set: {
+          deletedAt: new Date(),
+        },
+      },
+    )
+    return result.modifiedCount > 0
   }
 }
