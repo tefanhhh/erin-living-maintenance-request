1dc9ff8821174c6c3673434eafb5d93363ece4d4
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-20 00:59:43 +0700

    refactor server initialization to use ApolloServer with HTTP and WebSocket support

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,42 +1,88 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { readFileSync } from 'fs';
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
-import { Resolvers } from './graphql.type';
-import { DateScalar, ObjectIdScalar } from './scalars';
-import { maintenanceRequestResolvers } from './modules/maintenance-request/resolver';
-import dbConnection from './db/connection';
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import cors from 'cors'
+import { readFileSync } from 'fs'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { Resolvers } from './graphql.type'
+import { DateScalar, ObjectIdScalar } from './scalars'
+import { maintenanceRequestResolvers } from './modules/maintenance-request/resolver'
+import dbConnection from './db/connection'
+import { createServer } from 'http'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { WebSocketServer } from 'ws'
+import { useServer } from 'graphql-ws/lib/use/ws'
+import express from 'express'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const typeDefs = readFileSync(resolve(__dirname, '../src/', 'schema.graphql'), { encoding: 'utf-8' });
+async function bootstrap() {
+  const app = express()
+  const typeDefs = readFileSync(
+    resolve(
+      dirname(fileURLToPath(import.meta.url)),
+      '../src/',
+      'schema.graphql',
+    ),
+    { encoding: 'utf-8' },
+  )
 
-const resolvers: Resolvers = {
-  ObjectId: ObjectIdScalar,
-  Date: DateScalar,
-  Query: {
-    ...maintenanceRequestResolvers.Query,
-  },
-  Mutation: {
-    ...maintenanceRequestResolvers.Mutation,
+  const resolvers: Resolvers = {
+    ObjectId: ObjectIdScalar,
+    Date: DateScalar,
+    Query: {
+      ...maintenanceRequestResolvers.Query,
+    },
+    Mutation: {
+      ...maintenanceRequestResolvers.Mutation,
+    },
+    Subscription: {
+      ...maintenanceRequestResolvers.Subscription,
+    },
+  }
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+  try {
+    await dbConnection()
+    console.log('Connected to the database')
+
+    const httpServer = createServer(app)
+    const wsServer = new WebSocketServer({
+      server: httpServer,
+      path: '/',
+    })
+    const serverCleanup = useServer({ schema }, wsServer)
+    const server = new ApolloServer({
+      schema,
+      introspection: true,
+      plugins: [
+        ApolloServerPluginDrainHttpServer({ httpServer }),
+        {
+          async serverWillStart() {
+            return {
+              async drainServer() {
+                await serverCleanup.dispose()
+              },
+            }
+          },
+        },
+      ],
+    })
+
+    await server.start()
+    app.use(
+      '/',
+      cors<cors.CorsRequest>(),
+      express.json(),
+      expressMiddleware(server),
+    )
+
+    await new Promise<void>((resolve) =>
+      httpServer.listen({ port: 4000 }, resolve),
+    )
+    console.log(`🚀 Server ready at http://localhost:4000/`)
+  } catch (error) {
+    console.error('❌ Failed to connect to database:', error)
+    process.exit(1)
   }
 }
 
-try {
-  await dbConnection();
-  console.log('Connected to the database');
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
-} catch (error) {
-  console.error('❌ Failed to connect to database:', error);
-  process.exit(1);
-}
+await bootstrap()
