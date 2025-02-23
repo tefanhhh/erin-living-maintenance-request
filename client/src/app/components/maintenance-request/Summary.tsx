961f24a9bca6ef0d3e8fc69462267bd2289b4f0b
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-23 10:17:50 +0800

    feat: add MaintenanceRequestSummary component for displaying summary of maintenance requests

---
+++ client/src/app/components/maintenance-request/Summary.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import client from '@/lib/apollo.client'
+import {
+  MaintenanceRequestCreatedSubscription,
+  MaintenanceRequestResolvedSubscription,
+  SummaryMaintenanceRequestQuery,
+} from '@/gql/graphql'
+import {
+  maintenanceRequestCreated,
+  maintenanceRequestResolved,
+  summaryMaintenanceRequest,
+} from '@/gql-query/maintenance-request'
+import { useState, useEffect } from 'react'
+
+export default function MaintenanceRequestSummary() {
+  const summaryTitles = [
+    { title: 'Open Requests', key: 'open' },
+    { title: 'Urgent Requests', key: 'urgent' },
+    { title: 'Average time (days) to resolve', key: 'averageDaysToResolve' },
+  ] as const
+
+  const [summary, setSummary] = useState<
+    SummaryMaintenanceRequestQuery['summaryMaintenanceRequest']
+  >({})
+
+  useEffect(() => {
+    async function fetchSummary() {
+      const { data } = await client.query<SummaryMaintenanceRequestQuery>({
+        query: summaryMaintenanceRequest,
+      })
+      setSummary(data?.summaryMaintenanceRequest || {})
+    }
+    fetchSummary()
+
+    const maintenanceRequestCreatedSubscription =
+      client.subscribe<MaintenanceRequestCreatedSubscription>({
+        query: maintenanceRequestCreated,
+      })
+
+    maintenanceRequestCreatedSubscription.subscribe({
+      next(val) {
+        if (val?.data?.maintenanceRequestCreated) {
+          fetchSummary()
+        }
+      },
+      error(err) {
+        console.error('Subscription Error:', err)
+      },
+    })
+
+    const maintenanceRequestResolvedSubscription =
+      client.subscribe<MaintenanceRequestResolvedSubscription>({
+        query: maintenanceRequestResolved,
+      })
+
+    maintenanceRequestResolvedSubscription.subscribe({
+      next(val) {
+        if (val?.data?.maintenanceRequestResolved) {
+          fetchSummary()
+        }
+      },
+      error(err) {
+        console.error('Subscription Error:', err)
+      },
+    })
+  })
+
+  return (
+    <div className="flex items-center justify-center gap-4 flex-wrap mb-8">
+      {summaryTitles.map((it) => (
+        <div
+          key={it.title}
+          className="w-[90px] h-[90px] rounded-xl bg-white px-1 flex flex-col items-center justify-start pt-4"
+          style={{ boxShadow: '0px 6px 14px 0px rgba(0, 0, 0, 0.06)' }}
+        >
+          <h2 className="font-inter font-bold text-primary text-3xl text-center">
+            {summary?.[it.key] || 0}
+          </h2>
+          <p className="font-inter text-foreground text-[9px] text-center mb-0 leading-[10.89px]">
+            {it.title}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
