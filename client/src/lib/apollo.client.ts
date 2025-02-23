45cb8bb9f4f4bd6c9a9ebecfbd904a002ad5b256
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-23 10:18:20 +0800

    feat: implement WebSocket support for Apollo Client with split link for subscriptions

--- client/src/lib/apollo.client.ts
+++ client/src/lib/apollo.client.ts
@@ -1,10 +1,33 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { split, HttpLink } from '@apollo/client'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000',
+})
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:4000',
+  }),
+)
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink,
+)
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'http://localhost:4000',
-    credentials: 'same-origin',
-  }),
+  link: splitLink,
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
