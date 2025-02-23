6b1826885101277872ec7cc9ebeb52e6ebd9bc73
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-23 08:08:03 +0800

    feat: add placeholder prop to FormInput component for enhanced usability

--- client/src/app/components/form/Input.tsx
+++ client/src/app/components/form/Input.tsx
@@ -5,12 +5,19 @@ interface InputProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   register?: any
   className?: string
+  placeholder?: string
 }
 
-export default function FormInput({ name, register, className }: InputProps) {
+export default function FormInput({
+  name,
+  register,
+  className,
+  placeholder,
+}: InputProps) {
   return (
     <input
       {...(register ? register(name) : {})}
+      placeholder={placeholder}
       className={`form-input w-full p-2 rounded-xl px-4 py-[14px] ${className}`}
     />
   )
