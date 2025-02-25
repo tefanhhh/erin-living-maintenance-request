4c88c3939c4d03fe21480dbff723090ad7dd6ea6
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-26 04:27:51 +0700

    feat: add animation to maintenance request list items using framer-motion

--- client/src/components/maintenance-request/list/Index.tsx
+++ client/src/components/maintenance-request/list/Index.tsx
@@ -2,23 +2,30 @@
 import MaintenanceRequestItem from '@/components/maintenance-request/list/Item'
 import { RootState } from '@/stores/index.store'
 import { useSelector } from 'react-redux'
+import { motion } from 'framer-motion'
 
 export default function MaintenanceRequestListComponent() {
   const list = useSelector((state: RootState) => state.maintenanceRequest.list)
   return (
     <ul className="mt-4">
-      {list?.map((it, i) => {
-        return (
+      {list?.map((it, i) => (
+        <motion.li
+          key={`motion-${it._id}`}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.3, delay: i * 0.1 }}
+        >
           <MaintenanceRequestItem
-            key={i}
+            key={it._id}
             _id={it._id}
             title={it.title}
             urgency={it.urgency}
             status={it.status}
             createdAt={it.createdAt}
           />
-        )
-      })}
+        </motion.li>
+      ))}
     </ul>
   )
 }
