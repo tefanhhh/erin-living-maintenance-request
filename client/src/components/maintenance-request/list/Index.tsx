ac9e707c2562d9fa822ff4e2488372742a26fb87
Tefan Haetami <tefanhaetami87@gmail.com>
2025-02-26 03:52:14 +0700

    feat: add MaintenanceRequestListComponent to display maintenance requests

---
+++ client/src/components/maintenance-request/list/Index.tsx
@@ -0,0 +1,24 @@
+'use client'
+import MaintenanceRequestItem from '@/components/maintenance-request/list/Item'
+import { RootState } from '@/stores/index.store'
+import { useSelector } from 'react-redux'
+
+export default function MaintenanceRequestListComponent() {
+  const list = useSelector((state: RootState) => state.maintenanceRequest.list)
+  return (
+    <ul className="mt-4">
+      {list?.map((it, i) => {
+        return (
+          <MaintenanceRequestItem
+            key={i}
+            _id={it._id}
+            title={it.title}
+            urgency={it.urgency}
+            status={it.status}
+            createdAt={it.createdAt}
+          />
+        )
+      })}
+    </ul>
+  )
+}
