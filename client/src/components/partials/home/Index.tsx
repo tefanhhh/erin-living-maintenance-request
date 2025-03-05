5bb2618848452d28249bb7229855533ece0d5f83
Tefan Haetami <tefanhaetami87@gmail.com>
2025-03-05 14:05:23 +0700

    refactor: remove unused pagination and filter components, update list component to integrate filter functionality

--- client/src/components/partials/home/Index.tsx
+++ client/src/components/partials/home/Index.tsx
@@ -4,18 +4,24 @@ import MaintenanceRequestSummary from '@/components/partials/home/Summary'
 import MaintenanceRequestList from '@/components/partials/home/list/Index'
 import { useEffect } from 'react'
 import { RootState } from '@/lib/store'
-import { summary, findAll } from '@/lib/store/slices/maintenance-request'
+import {
+  summary,
+  findAll,
+  actions,
+} from '@/lib/store/slices/maintenance-request'
 import { Button, Link } from '@heroui/react'
 import MaintenanceRequestSubscription from '@/components/partials/home/Subscription'
-import MaintenanceRequestFilter from '@/components/partials/home/Filter'
-import MaintenanceRequestPagination from '@/components/partials/home/Pagination'
 import { useAppDispatch, useAppSelector } from '@/lib/store/hooks'
+import { useSearchParams } from 'next/navigation'
+import { Sort } from '@/lib/gql/graphql'
 
 export default function MaintenanceRequestComponent() {
   const queryParam = useAppSelector(
     (state: RootState) => state.maintenanceRequest.queryParam,
   )
   const dispatch = useAppDispatch()
+  const searchParams = useSearchParams()
+  const { setQueryParam } = actions
 
   useEffect(() => {
     dispatch(summary())
@@ -25,6 +31,26 @@ export default function MaintenanceRequestComponent() {
     dispatch(findAll())
   }, [dispatch, queryParam])
 
+  useEffect(() => {
+    const updatedQueryParam = { ...queryParam }
+
+    if (searchParams.get('keyword')) {
+      updatedQueryParam.keyword = searchParams.get('keyword')!
+    }
+
+    if (searchParams.get('sort')) {
+      updatedQueryParam.sort = searchParams.get('sort') as Sort
+    }
+
+    if (searchParams.get('page')) {
+      updatedQueryParam.page = Number(searchParams.get('page'))
+    }
+
+    if (JSON.stringify(updatedQueryParam) !== JSON.stringify(queryParam)) {
+      dispatch(setQueryParam(updatedQueryParam))
+    }
+  }, [searchParams])
+
   return (
     <div className="container mx-auto px-4 sm:px-0">
       <div className="py-16">
@@ -33,9 +59,7 @@ export default function MaintenanceRequestComponent() {
         </h1>
         <MaintenanceRequestSubscription />
         <MaintenanceRequestSummary />
-        <MaintenanceRequestFilter />
         <MaintenanceRequestList />
-        <MaintenanceRequestPagination />
       </div>
       <div className="flex items-center justify-end gap-4 sticky left-0 bottom-6 z-10">
         <Link href="/update">
--- client/src/components/partials/home/list/Index.tsx
+++ client/src/components/partials/home/list/Index.tsx
@@ -3,31 +3,34 @@ import MaintenanceRequestItem from '@/components/partials/home/list/Item'
 import { RootState } from '@/lib/store'
 import { motion } from 'framer-motion'
 import { useAppSelector } from '@/lib/store/hooks'
+import MaintenanceRequestFilter from './Filter'
 
 export default function MaintenanceRequestListComponent() {
   const list = useAppSelector(
     (state: RootState) => state.maintenanceRequest.list,
   )
   return (
-    <ul className="mt-4">
-      {list.items.map((it, i) => (
-        <motion.li
-          key={`motion-${it._id}`}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.3, delay: i * 0.1 }}
-        >
-          <MaintenanceRequestItem
-            key={it._id}
-            _id={it._id}
-            title={it.title}
-            urgency={it.urgency}
-            status={it.status}
-            createdAt={it.createdAt}
-          />
-        </motion.li>
-      ))}
-    </ul>
+    <MaintenanceRequestFilter>
+      <ul className="mt-4">
+        {list.items.map((it, i) => (
+          <motion.li
+            key={`motion-${it._id}`}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.3, delay: i * 0.1 }}
+          >
+            <MaintenanceRequestItem
+              key={it._id}
+              _id={it._id}
+              title={it.title}
+              urgency={it.urgency}
+              status={it.status}
+              createdAt={it.createdAt}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </MaintenanceRequestFilter>
   )
 }
